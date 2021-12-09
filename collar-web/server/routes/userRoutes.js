b8c92778588297a199a68e9928056ed4f5bdce10
kotaewing <kota.ewing@gmail.com>
2021-12-08 21:04:32 -0700

    added express

---
+++ collar-web/server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { createUser, updateEmployee } = require('../controllers/userController');
+
+router.route('/').post(createUser);
+
+router.route('/:userId').put(updateEmployee)
+
+
+module.exports = router;
